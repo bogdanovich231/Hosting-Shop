afc53284046e809ce901e0c21c0e9ef003416768
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2024-06-26 14:08:22 +0200

    feat: connect adapter to items activity

--- app/src/main/java/com/example/hosting_shop/ItemsActivity.kt
+++ app/src/main/java/com/example/hosting_shop/ItemsActivity.kt
@@ -2,6 +2,7 @@ package com.example.hosting_shop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class ItemsActivity : AppCompatActivity() {
@@ -16,5 +17,7 @@ class ItemsActivity : AppCompatActivity() {
         items.add(Item(2, "mar2", "rating", "Alaskan", "Thunderfuck\n" + "Strain", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", "10g /", 100))
         items.add(Item(3, "mar3", "rating", "Alaskan", "Thunderfuck\n" + "Strain", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", "10g /", 100))
 
+        itemsList.layoutManager = LinearLayoutManager(this)
+        itemsList.adapter = itemsAdapter(items, this)
     }
 }
